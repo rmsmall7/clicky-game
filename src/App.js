a954c3b036cb7056a30eb0be31f874a37e32f559
unknown <rmsmall7@gmail.com>
2019-06-07 23:49:13 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -53,39 +53,6 @@ class App extends Component {
     }
   };
 
-  // gameOver = () => {
-  //   if (this.state.score > this.state.highscore) {
-  //     this.setState({ highscore: this.state.score }, function () {
-  //       console.log(this.state.highscore);
-  //     });
-  //   }
-  //   this.state.cards.forEach(card => {
-  //     card.count = 0;
-  //   });
-  //   alert(`Game Over :( \nscore: ${this.state.score}`);
-  //   this.setState({ score: 0 });
-  //   return true;
-  // }
-
-  // clickCount = id => {
-  //   this.state.cards.find((o, i) => {
-  //     if (o.id === id) {
-  //       if (cards[i].count === 0) {
-  //         cards[i].count = cards[i].count + 1;
-  //         this.setstate({ score: this.state.score + 1 }, function () {
-  //           console.log(this.state.score);
-  //         });
-  //         this.state.cards.sort(() => Math.random() - 0.5)
-  //         return true;
-  //       } else {
-  //         this.gameOver();
-  //       }
-  //     }
-  //     return false;
-  //   });
-  // }
-
-
   render() {
     return (
       <Wrapper>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "../Card/Card.css";
-// import "./cards.js";
-// import "../../cards.json";
-// import "../../App.js";
 
 
 
